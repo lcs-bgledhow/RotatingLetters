821048977c311af0b287342d928b49ba72e7b43b
Ben Gledhow <bgledhow@lcs-mba3210.lcs.on.ca>
2018-04-10 15:22:45 -0400

    More progress on Rotating Letters

--- RotatingLetters/main.swift
+++ RotatingLetters/main.swift
@@ -9,9 +9,9 @@
 import Foundation
 
 // INPUT
-
 // Loop forever until we get good input
-while 1 == 1 {
+var validInput = "" // Outside the loop -- we can use it later
+prompt: while 1 == 1 {
     
     // Prompt the user
     print("Input word is? ")
@@ -34,12 +34,31 @@ while 1 == 1 {
    
     }
     
+    // Test #3: Check to ensure there is only UPPERCASE letters and no spaces
+    checking: for individualCharacter in givenInput {
+        
+        // Print the character were looking at
+        print(individualCharacter)
+        
+        // Check the character
+        switch individualCharacter {
+        case "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" :
+            continue checking   // OK go to next character in the string
+        default:
+            continue prompt     // Stop looking, we've found bad input
+            
+        }
+        
+    }
+    
     // If we got to this point we know the input is good
+    validInput = givenInput
     break    // quit the while loop and begin the PROCESS section
 
 }
 
-
+// PROCESS
+print(validInput)
 
 
 
